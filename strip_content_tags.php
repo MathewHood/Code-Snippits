addd186f5a2f8512dc4cae6f47d042d98a28e5b6
Mathew Hood <mat@sennza.com.au>
2012-03-02 15:24:26 +1000

    Added comments to strip content tags snippit

--- strip_content_tags.php
+++ strip_content_tags.php
@@ -3,13 +3,13 @@
 Place this code in your functions.php file to strip shortcode, img tags, divs, objects, blockquotes and tables from appearing before the <!--more--> tag.
 */
 function wpt_strip_content_tags($content) {
-    $content = strip_shortcodes($content);
+    $content = strip_shortcodes($content); /* Strip shortcodes */
     $content = str_replace(']]>', ']]&gt;', $content);
-    $content = preg_replace('/<img[^>]+./','',$content);
-    $content = preg_replace('%<div.+?</div>%is', '', $content);
-    $content = preg_replace('%<object.+?</object>%is', '', $content);
-	$content = preg_replace('/<blockquote>/is', '', $content);
-	$content = preg_replace('%<table.+?</table>%is', '', $content);
+    $content = preg_replace('/<img[^>]+./','',$content); /* Remove img tags */
+    $content = preg_replace('%<div.+?</div>%is', '', $content); /* Removes div tags */
+    $content = preg_replace('%<object.+?</object>%is', '', $content); /* Removes object tags */
+	$content = preg_replace('/<blockquote>/is', '', $content); /* Removes block quotes */
+	$content = preg_replace('%<table.+?</table>%is', '', $content); /* Removes tables */
     return $content;
 }
 
